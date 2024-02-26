1c84feff852c20ab94a91deba1cff47ae1770e3d
Joel Herrero <joelv2@gmail.com>
2024-02-26 16:58:05 +0100

    images to assets folder

--- src/data/imageMock.ts
+++ src/data/imageMock.ts
@@ -3,70 +3,70 @@ import { IImage } from "../interfaces/image.interface";
 export const imageMock: IImage[] = [
   {
     imageUrl:
-      "../src/data/images/1681896083693-cats-animals-domestic-cat-eyes-wallpaper-9105.jpeg",
+      "../src/assets/images/1681896083693-cats-animals-domestic-cat-eyes-wallpaper-9105.jpeg",
     title: "Looking up",
     id: 330620590203624,
   },
   {
     imageUrl:
-      "../src/data/images/1681896122726-photo-1582630662855-8d2dbc0ee144-938d.jpeg",
+      "../src/assets/images/1681896122726-photo-1582630662855-8d2dbc0ee144-938d.jpeg",
     title: "Sleeping cat",
     id: 923943398550795,
   },
   {
-    imageUrl: "../src/data/images/1681896139309-imagecats-b10f1.jpeg",
+    imageUrl: "../src/assets/images/1681896139309-imagecats-b10f1.jpeg",
     title: "So cute",
     id: 774110623284355,
   },
   {
     imageUrl:
-      "../src/data/images/1681896149284-photo-1542652735873-fb2825bac6e2-ff101.jpeg",
+      "../src/assets/images/1681896149284-photo-1542652735873-fb2825bac6e2-ff101.jpeg",
     title: "Lion king",
     id: 747044110266795,
   },
   {
-    imageUrl: "../src/data/images/1681896160669-imagesca-1b1d.jpeg",
+    imageUrl: "../src/assets/images/1681896160669-imagesca-1b1d.jpeg",
     title: "Deep",
     id: 428382243542093,
   },
   {
     imageUrl:
-      "../src/data/images/1681896310279-220726-cat-theo-elise-ew-636p-6cd3b0-1750.jpeg",
+      "../src/assets/images/1681896310279-220726-cat-theo-elise-ew-636p-6cd3b0-1750.jpeg",
     title: "Bored",
     id: 945126011055127,
   },
   {
-    imageUrl: "../src/data/images/1681896188498-1_5978274-0a72.jpeg",
+    imageUrl: "../src/assets/images/1681896188498-1_5978274-0a72.jpeg",
     title: "Walking cat",
     id: 299141467161153,
   },
   {
     imageUrl:
-      "../src/data/images/1681896149284-photo-1542652735873-fb2825bac6e2-ff101.jpeg",
+      "../src/assets/images/1681896149284-photo-1542652735873-fb2825bac6e2-ff101.jpeg",
     title: "On top",
     id: 884866930834537,
   },
   {
     imageUrl:
-      "../src/data/images/1681896096618-cb3160962a9a5f25e470d6bcb84c351b-ebb3.jpeg",
+      "../src/assets/images/1681896096618-cb3160962a9a5f25e470d6bcb84c351b-ebb3.jpeg",
     title: "Elegant",
     id: 684273933819685,
   },
   {
     imageUrl:
-      "../src/data/images/1681896219451-gettyimages-1189806758_custom-0a0266b3e82cfd9a1bb6016795a1a51b87277273-47e8.jpeg",
+      "../src/assets/images/1681896219451-gettyimages-1189806758_custom-0a0266b3e82cfd9a1bb6016795a1a51b87277273-47e8.jpeg",
     title: "Shower time",
     id: 788853574764268,
   },
   {
     imageUrl:
-      "../src/data/images/1681901069614-Thinking-of-getting-a-cat-be2d.png",
+      "../src/assets/images/1681901069614-Thinking-of-getting-a-cat-be2d.png",
     title: "Awaiting",
     id: 755050511993285,
   },
   {
     imageUrl:
-      "../src/data/images/1681896198691-photo-1586164383881-d01525b539d6-4449.jpeg",
+      "../src/assets/images/1681896198691-photo-1586164383881-d01525b539d6-4449.jpeg",
     title: "Gardfield",
     id: 211146244090724,
   },
